413d1f0c0d7bb0b650b00987d7810daf77284dea
Raymond Nguyen <04hng@RaymondsMacBook.attlocal.net>
2023-09-06 12:01:21 -0700

    set up portfolio with all links and project db

--- db/projects.js
+++ db/projects.js
@@ -1,39 +1,59 @@
+import CodeQuiz from "./CodeQuiz.png";
+import fashionFusion from "./fashionFusion.png";
+import memeGenerator from "./memeGenerator.png";
+import noteApp from "./noteApp.png";
+import travelJournal from "./travelJournal.png";
+import weatherDashboard from "./weatherDashboard.png";
+
 const projects = [
   {
     title: "Fashion Fusion",
     description: "This is project 1",
-    imgUrl: "",
-    url: "",
+    imgUrl: fashionFusion,
+    url: "https://fashion-fusion-fdbc9c87836b.herokuapp.com/",
+    githubUrl: "https://github.com/itcreativeusa/fashion-fusion",
+    description: "Fashion-Fusion is an eCommerce platform specializing in clothing, where garments are organized by colors. This App was built using MERN stack.",
   },
   {
     title: "Note-App",
     description: "This is project 2",
-    imgUrl: "",
-    url: "",
+    imgUrl: noteApp,
+    url: "https://note-master-714-cdec4815ebd5.herokuapp.com/",
+    githubUrl: "https://github.com/Fenaum/note-take-app",
+    description: "Note-App is a responsive note taking application using Express.js to apply the CRUD operation.",
   },
   {
     title: "Meme Generator",
     description: "This is project 1",
-    imgUrl: "",
-    url: "",
+    imgUrl: memeGenerator,
+    url: "https://memesify-be2bdd38706f.herokuapp.com//",
+    githubUrl: "https://github.com/Fenaum/meme-generator",
+    description: "A meme generator to create customized memes, This app was built using React and a public Meme API to fetch the meme data.",
   },
   {
+    description: "",
     title: "Weather Dashboard",
     description: "This is project 2",
-    imgUrl: "",
-    url: "",
+    imgUrl: weatherDashboard,
+    url: "https://fenaum.github.io/weather-dashboard-api/",
+    githubUrl: "https://github.com/Fenaum/weather-dashboard-api",
+    description: "A basic JavaScript application that generate weather information based on city using Open Weather API.",
   },
   {
     title: "Coding Quiz Game",
     description: "This is project 1",
-    imgUrl: "",
-    url: "",
+    imgUrl: CodeQuiz,
+    url: "https://fenaum.github.io/coding-quiz-uh/",
+    githubUrl: "https://github.com/Fenaum/coding-quiz-uh",
+    description: "A basic Coding Quiz App using Vanilla Javascript.",
   },
   {
-    title: "Project 2",
+    title: "Travel Journal",
     description: "This is project 2",
-    imgUrl: "",
+    imgUrl: travelJournal,
     url: "https://travel-journal-react-342a400076d8.herokuapp.com/",
+    githubUrl: "https://github.com/Fenaum/react-travel-journal",
+    description: "A travel Journal Application built through React and Javascript.",
   },
 ];
 
--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,19 +1,30 @@
 import './Portfolio.css'
 import projects from '../../../db/projects'
+import githubImg from '../../assets/github.svg'
+import externalLink from '../../assets/external-link.svg'
 
 export default function Portfolio() {
-    return (
-        <a href={projects.imgUrl} className="portfolio-container">
-            <div className="portfolio-grid">
-                {projects.map((project, index) => (
-                    <div key={index} className="project-card">
-                        <h3>{project.title}</h3>
-                        <p>{project.description}</p>
-                        <a></a>
-                        {/* Add more project details here */}
-                    </div>
-                ))}
+    const projectCards = projects.map((project, index) => {
+        return (
+            <div key={index} className="project-card">
+                <h3>{project.title}</h3>
+                <img className='card-img' src={project.imgUrl} alt='project image' ></img>
+                <p className='projectDescription'>{project.description}</p>
+                <a className='githublink' href={project.githubUrl} target='_blank' >
+                    Github <img src={githubImg} alt='github icon' ></img>
+                </a>
+                <a className='projectLink' href={project.url} target='_blank' >
+                    App <img src={externalLink} alt='External Link' ></img>
+                </a>
             </div>
-        </a>
+        )
+    })
+
+    return (
+        <div className="portfolio-container">
+            <div className="portfolio-grid">
+                {projectCards}
+            </div>
+        </div>
     )
 }
