8ebe67058146be4bf7c3f55976438f845a061303
Raymond Nguyen <04hng@RaymondsMacBook.attlocal.net>
2023-09-05 19:44:27 -0700

    added procfile

--- db/projects.js
+++ db/projects.js
@@ -1,40 +1,40 @@
 const projects = [
-    {
-        title: "Fashion Fusion",
-        description: "This is project 1",
-        imgUrl: "",
-        url: "",
-    },
-    {
-        title: "Note-App",
-        description: "This is project 2",
-        imgUrl: "",
-        url: "",
-    },
-    {
-        title: "Meme Generator",
-        description: "This is project 1",
-        imgUrl: "",
-        url: "",
-    },
-    {
-        title: "Weather Dashboard",
-        description: "This is project 2",
-        imgUrl: "",
-        url: "",
-    },
-    {
-        title: "Coding Quiz Game",
-        description: "This is project 1",
-        imgUrl: "",
-        url: "",
-    },
-    {
-        title: "Project 2",
-        description: "This is project 2",
-        imgUrl: "",
-        url: "",
-    },
+  {
+    title: "Fashion Fusion",
+    description: "This is project 1",
+    imgUrl: "",
+    url: "",
+  },
+  {
+    title: "Note-App",
+    description: "This is project 2",
+    imgUrl: "",
+    url: "",
+  },
+  {
+    title: "Meme Generator",
+    description: "This is project 1",
+    imgUrl: "",
+    url: "",
+  },
+  {
+    title: "Weather Dashboard",
+    description: "This is project 2",
+    imgUrl: "",
+    url: "",
+  },
+  {
+    title: "Coding Quiz Game",
+    description: "This is project 1",
+    imgUrl: "",
+    url: "",
+  },
+  {
+    title: "Project 2",
+    description: "This is project 2",
+    imgUrl: "",
+    url: "https://travel-journal-react-342a400076d8.herokuapp.com/",
+  },
 ];
 
 export default projects;
